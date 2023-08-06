e630659261cdcecdbb91331e3d79bb5a4c2d7a45
KaterynaChaplynska <101507992+KaterynaChaplynska@users.noreply.github.com>
2023-08-06 17:14:51 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,31 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+
+
+function createGallaryItem(array) {
+    return array
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+  
+`;
+        })
+        .join("");
+}
+
+const galleryItemsMarcup = createGallaryItem(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarcup);
+
+let gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    showCounter: false,
+});
