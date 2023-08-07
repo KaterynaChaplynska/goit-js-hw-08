269f3ce63e368d4016807b95f3283b28e0c4dfa2
KaterynaChaplynska <101507992+KaterynaChaplynska@users.noreply.github.com>
2023-08-07 06:39:44 +0300

    02-03 video_feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+const playbackTime = localStorage.getItem(STORAGE_KEY);
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+if (!playbackTime) {
+    player.setCurrentTime(0);
+} else {
+    player.setCurrentTime(playbackTime);
+}
+
+function currentTime(evt) {
+    const currentPlaybackTime = evt.seconds;
+    localStorage.setItem(STORAGE_KEY, currentPlaybackTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+const STORAGE_KEY = 'feedback-form-state';
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+let formData = savedData || {};
+
+getFormData();
+
+function onFormData(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    if (!evt.target.elements[0].value || !evt.target.elements[1].value) {
+        return;
+    }
+    console.log(formData);
+    formData = {};
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function getFormData() {
+    if (savedData) {
+        for (const key in savedData) {
+            if (savedData.hasOwnProperty(key)) {
+                input.value = savedData.email || ``;
+                textarea.value = savedData.message || ``;
+            }
+        }
+    }
+}
